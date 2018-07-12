09698c4a41a63eed7b6bdbb1b2ee3d3cc3824565
unknown <josenogueira95@hotmail.com>
2018-07-13 00:19:47 +0100

    update

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+        Folklore\Image\ImageServiceProvider::class,
 
 
     ],
@@ -210,7 +210,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        'Image' => Folklore\Image\Facades\Image::class,
         
     ],
 
